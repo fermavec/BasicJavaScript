4e6875bb0e16d2b61cc050d42fb1d7d5f8d1efec
Fernando Mavec <fermavec@gmail.com>
2022-02-05 12:18:20 -0600

    Archivos de Basic JavaScript

---
+++ valores.js
@@ -0,0 +1,21 @@
+/*PRIMITIVOS*/ * /
+//Numbers
+40
+
+//Strings
+    "Fernando Chavez"
+
+//booleanos
+true
+false
+
+//empty values
+null
+undefined
+
+/*NO PRIMITIVOS*/
+//array
+    [0, 2, 6, 8, 4]
+
+//tipo Objeto
+{ nombre: "Fer" }
